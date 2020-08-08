0914dbf42f5e087d8241fce17f7497e97b8d3724
Saran Sasidharan <saran.s.dhar@gmail.com>
2020-08-08 13:33:02 +0530

    Port/host is read optionally from argument addr

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,15 +19,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	addr := flag.String("addr", ":8080", "The address of the application")
+	flag.Parse()
+
 	r := newRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
+
 	go r.run()
-	err := http.ListenAndServe(":8080", nil)
+
+	log.Println("Starting web server on", *addr)
+	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
